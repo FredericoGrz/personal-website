b3efbda6f3dc984fd50a9393927aac417bf7be77
FredericoGrz <frederico.graz@gmail.com>
2024-06-27 16:52:14 -0300

    Adicionado o componente Primary

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { Primary } from "./components/Primary";
 
 function App() {
   return (
     <div className="w-full h-screen bg-zinc-50 font-poppins">
       <Header />
+      <Primary />
     </div>
   );
 }
---
+++ src/components/Primary.tsx
@@ -0,0 +1,50 @@
+import { FaLinkedin } from "react-icons/fa6";
+import { FaGithubSquare } from "react-icons/fa";
+export function Primary() {
+  return (
+    <div className="flex flex-col gap-4 w-full items-center pt-20 lg:flex-row lg:justify-center lg:gap-20 lg:pt-44">
+      <div>
+        <img
+          src="https://github.com/fredericogrz.png"
+          alt=""
+          className="w-56 h-56 rounded-full lg:w-96 lg:h-96 transition-all duration-500 ease-in-out"
+        />
+      </div>
+      <div className="flex flex-col gap-4 lg:gap-6 items-center tracking-wider">
+        <div className="flex flex-col gap-2 text-center">
+          <p className="text-lg lg:text-xl text-zinc-500 transition-all duration-500 ease-in-out">
+            Hello, I'm
+          </p>
+          <p className="text-3xl lg:text-4xl font-bold transition-all duration-500 ease-in-out">
+            Antonio Gomes
+          </p>
+          <p className="text-xl lg:text-2xl text-zinc-500 font-bold transition-all duration-500 ease-in-out">
+            Frontend Developer
+          </p>
+        </div>
+        <button
+          type="button"
+          className="border-2 border-zinc-700 w-fit p-2 lg:text-lg rounded-xl hover:scale-110 transition-transform"
+        >
+          Contact Info
+        </button>
+        <div className="flex gap-2 items-center">
+          <a
+            className="w-fit hover:scale-105 transition-transform"
+            href="https://www.linkedin.com/in/antoniofggomes/"
+            target="_blank"
+          >
+            <FaLinkedin className="text-2xl lg:text-3xl transition-all duration-500 ease-in-out" />
+          </a>
+          <a
+            className="w-fit hover:scale-105 transition-transform"
+            href="https://www.github.com/fredericogrz"
+            target="_blank"
+          >
+            <FaGithubSquare className="text-2xl lg:text-3xl transition-all duration-500 ease-in-out" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
