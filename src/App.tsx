e5671f453d65acce969c71d1e0839267ff97aa1b
FredericoGrz <frederico.graz@gmail.com>
2024-06-27 20:33:19 -0300

    Few layout updates

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { Primary } from "./components/Primary";
 
 function App() {
   return (
-    <div className="w-full h-screen bg-zinc-50 font-poppins">
-      <Header />
-      <Primary />
+    <div className="w-full min-h-screen bg-zinc-50 font-poppins">
+      <div className="h-screen">
+        <Header />
+        <Primary />
+      </div>
     </div>
   );
 }
