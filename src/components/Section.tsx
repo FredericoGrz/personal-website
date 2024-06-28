d97665958a0689f3ef4c4568c0eed4a9438d0303
FredericoGrz <frederico.graz@gmail.com>
2024-06-28 12:51:39 -0300

    Alteração semântica, troca de div para section

---
+++ src/components/Section.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type SectionProps = {
+  children: React.ReactNode;
+  id?: string;
+  className?: string;
+};
+
+export function Section({ id, children, className = "" }: SectionProps) {
+  return (
+    <section id={id} className={`h-screen ${className}`}>
+      {children}
+    </section>
+  );
+}
